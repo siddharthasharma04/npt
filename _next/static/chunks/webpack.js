8be16683cc835995db93f930cb9089f6ac3566af
siddharthasharma04 <siddharthasharma04@outlook.com>
2021-07-31 23:12:25 +0530

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "14319defa9ee46f62e95"; }
+/******/ 		__webpack_require__.h = function() { return "64c80b6a490e35d39f73"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
